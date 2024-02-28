8580c4d08ddde204c41a904c9b3e166a1dc0dc10
Thor_ĐiCùngA <153697284+hieucaidu@users.noreply.github.com>
2024-02-28 19:55:05 +0700

    Add files via upload
    nonnn

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Em';
+const giftUrl = 'https://github.com/hieucaidu/hieucaidu.git';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Tặng người ấy';
+const contentCard = 'Chúc honey của anh 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+
+
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
